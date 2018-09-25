d9a3a62e27c6fb93631a5c250e2c78c7be35ff75
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-25 14:33:06 +0100

    committing creation of database and columns

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,11 +1,13 @@
 feature 'Viewing the bookmarks' do
   scenario 'visiting the index page' do
     visit('/')
+    expect(page.status_code).to eq(200)
     expect(page).to have_content('Bookmark-manager')
   end
 
   scenario 'display bookmarks list' do
     visit('/bookmarks')
+    expect(page.status_code).to eq(200)
     # click_button 'View Bookmark List'
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
